7dec09ff2a7edd64d221f53e3e92ccda02675077
maymurph <airam.g.jordan@gmail.com>
2024-01-30 14:58:42 +0100

    Add folders

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>To Do List</title>
+</head>
+<body>
+    <div class="main-section">
+        <div class="add-section">
+            <form action="">
+                <input type="text" 
+                        name="title" 
+                        placeholder="This field required"/>
+                <button type="submit">Add &nbsp; <span>&#43;</span></button>
+            </form>
+        </div>
+        <div class="show-todo-section">
+        <div class="todo-item">
+            <input type="checkbox">
+            <h2>This is</h2>
+            <br>
+            <small>created: 30/01/24</small>
+        </div>    
+           
+        </div>
+    </div>
+</body>
+</html>
