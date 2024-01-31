4af63a51be73c550da48fa20c94143e454dc34de
maymurph <airam.g.jordan@gmail.com>
2024-01-31 14:24:20 +0100

    Add images

--- db_conn.php
+++ db_conn.php
@@ -1,15 +1,14 @@
-<?php
+<?php 
 
 $sName = "localhost";
 $uName = "root";
-$pass ="";
+$pass = "";
 $db_name = "to_do_list";
 
 try {
-    $conn = new PDO("mysql :host=$sName;db_name=$db_name",
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
                     $uName, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
 }catch(PDOException $e){
-    echo "Connection failed : ". $e->getMessage();
+  echo "Connection failed : ". $e->getMessage();
 }
