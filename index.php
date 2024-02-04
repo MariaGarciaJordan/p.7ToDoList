ad92a5e08e61c992433d4afe90b28f635b803134
maymurph <airam.g.jordan@gmail.com>
2024-02-04 13:47:01 +0100

    Add filter complete or incomplete

--- index.php
+++ index.php
@@ -11,54 +11,60 @@ require 'db_conn.php';
     <link rel="stylesheet" href="View/css/style.css">
 </head>
 <body>
-   
     <div class="main-section">
-       <div class="add-section">
-          <form action="Controller/add.php" method="POST" autocomplete="off">
-             <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
-                <input type="text" 
-                     name="title" 
-                     style="border-color: #ff6666"
-                     placeholder="This field is required" />
-                <input type="text" 
-                     name="description" 
-                     placeholder="Describe your task" />
-                <select name="priority">
-                    <option value="High">High</option>
-                    <option value="Low">Low</option>
-                </select>
-                <button type="submit">Add &nbsp; <span>&#43;</span></button>
-
-             <?php } else { ?>
-                <input type="text" 
-                     name="title" 
-                     placeholder="What do you need to do?" />
-                <input type="text" 
-                     name="description"
-                     placeholder="Describe your task" />
-                <select name="priority">
-                    <option value="1">High</option>
-                    <option value="0">Low</option>
-                </select>
-                <button type="submit">Add &nbsp; <span>&#43;</span></button>
-             <?php } ?>
-          </form>
-       </div>
-       <?php 
-          $order = isset($_GET['order']) ? $_GET['order'] : 'id';
-          if ($order === 'priority') {
-              $todos = $conn->query("SELECT * FROM todos ORDER BY priority DESC");
-          } elseif ($order === 'date') {
-              $todos = $conn->query("SELECT * FROM todos ORDER BY date_time ASC, title ASC");
-          } else {
-              $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
-          }
-          
-       ?>
+        <div class="add-section">
+            <form action="Controller/add.php" method="POST" autocomplete="off">
+                <?php if(isset($_GET['mess']) && $_GET['mess'] == 'error') { ?>
+                    <input type="text" 
+                        name="title" 
+                        style="border-color: #ff6666"
+                        placeholder="This field is required" />
+                    <input type="text" 
+                        name="description" 
+                        placeholder="Describe your task" />
+                    <select name="priority">
+                        <option value="High">High</option>
+                        <option value="Low">Low</option>
+                    </select>
+                    <button type="submit">Add &nbsp; <span>&#43;</span></button>
+                    
+                <?php } else { ?>
+                    <input type="text" 
+                        name="title" 
+                        placeholder="What do you need to do?" />
+                    <input type="text" 
+                        name="description"
+                        placeholder="Describe your task" />
+                    <select name="priority">
+                        <option value="1">High</option>
+                        <option value="0">Low</option>
+                    </select>
+                    <button type="submit">Add &nbsp; <span>&#43;</span></button>
+                <?php } ?>
+            </form>
+        </div>
+        <?php 
+            $order = isset($_GET['order']) ? $_GET['order'] : 'id';
+            
+            if ($order === 'priority') {
+                $todos = $conn->query("SELECT * FROM todos ORDER BY priority DESC");
+            } elseif ($order === 'date') {
+                $todos = $conn->query("SELECT * FROM todos ORDER BY date_time ASC, title ASC");
+            } elseif ($order === 'completed') {
+                $todos = $conn->query("SELECT * FROM todos WHERE checked = 1 ORDER BY checked ASC");
+            } elseif ($order === 'incomplete') {
+                $todos = $conn->query("SELECT * FROM todos WHERE checked = 0");
+            } else {
+                $todos = $conn->query("SELECT * FROM todos ORDER BY id DESC");
+            }  
+        ?>
        <div class="show-todo-section">
        <form action="index.php" method="GET">
            <button type="submit" name="order" value="priority">Order by Priority</button>
            <button type="submit" name="order" value="date">Order by Date</button>
+           <button type="submit" name="order" value="completed">Show Completed</button>
+            <button type="submit" name="order" value="incomplete">Show Incomplete</button>
+
        </form>
 
             <?php if ($todos->rowCount() <= 0) { ?>
@@ -110,6 +116,6 @@ require 'db_conn.php';
 
     <script src="View/js/jquery-3.2.1.min.js"></script>
     <script src="View/js/functions.js"></script>
-    
+
 </body>
 </html>
